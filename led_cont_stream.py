e99072da655bc9db6a758e8d3b29d0ede40c60bd
try20240422 <167757295+try20240422@users.noreply.github.com>
2024-04-25 21:55:03 +0900

    Add files via upload

---
+++ led_cont_stream.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import requests
+
+st.title('LED Controller')
+
+node_mcu_ip = "192.168.1.240"  # NodeMcuのIPアドレスに置き換えてください
+
+def send_command(path):
+    url = f"http://{node_mcu_ip}:81{path}"  # ポート番号をURLに含める
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            st.success('Command was successful')
+        else:
+            st.error('Failed to execute command')
+    except Exception as e:
+        st.error(f'Error: {str(e)}')
+
+if st.button('Turn LED ON'):
+    send_command("/led/on")
+
+if st.button('Turn LED OFF'):
+    send_command("/led/off")
